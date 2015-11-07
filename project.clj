ee5420fc1099c5dd65e4ac83e6727e1e09b192e9
Ricardo J. Mendez <ricardo@numergent.com>
2015-11-07 20:43:39 +0000

    Bumping dependencies
    Upgrading cljsbuild and lein-cljsbuild dependenices

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject lein-chromebuild "0.3.0"
+(defproject lein-chromebuild "0.3.1"
   :description "A leiningen plugin for building Chrome browser extensions"
   :url "https://github.com/clumsyjedi/lein-chromebuild"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[lein-cljsbuild "1.0.1"]
-                 [cljsbuild "1.0.3"]
+  :dependencies [[lein-cljsbuild "1.1.1"]
+                 [cljsbuild "1.1.1"]
                  [juxt/dirwatch  "0.2.2"]])
