30e871044c11a6ed5812da665f8fa52e09c422c3
Ricardo J. Mendez <ricardo@numergent.com>
2016-09-16 17:41:51 +0300

    Adding group, bumping version to push to clojars

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject lein-chromebuild "0.3.1"
-  :description "A leiningen plugin for building Chrome browser extensions"
+(defproject org.clojars.ricardojmendez/lein-chromebuild "0.3.2"
+  :description "A leiningen plugin for building Chrome browser extensions."
   :url "https://github.com/clumsyjedi/lein-chromebuild"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
